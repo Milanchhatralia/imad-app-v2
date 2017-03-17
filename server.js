f3c8e47bcfaf0d48bc8f06a58381d843489970b7
Milanchhatralia <milan.chhatralia@gmail.com>
2017-03-18 00:00:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -87,7 +87,7 @@ app.get('/', function (req, res) {
 app.get('/:articleName',function(req, res){
     //articleName = article-one
     //articles[articleName] = content object for aticle one
-    var articleName = req.params.atricleName;
+    var articleName = req.params.articleName;
     res.send(createTemplate(article[articleName]));
 });
 
