9b9b450ced847ee609ffbc4639ad52f09e7eb00d
Milanchhatralia <milan.chhatralia@gmail.com>
2017-03-18 01:02:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,5 @@
 console.log('Loaded!');
-var img = document.getElementById("madi");
+var img = document.getElementById('madi');
 var moveLeft = 0;
 function moveRight() {
     moveLeft = moveLeft + 1;
