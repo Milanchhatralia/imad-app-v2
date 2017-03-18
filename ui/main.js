d65260b7be820675141d744f5e22ae2048d1438e
Milanchhatralia <milan.chhatralia@gmail.com>
2017-03-18 16:07:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,3 +20,26 @@ button.onclick = function (){
   request.open('GET','http://milanchhatralia.imad.hasura-app.io/counter', true);
   request.send(null);
 };
+
+var nameInput = document.getElementById('namein');
+var names = nameInput.value;
+var submit = document.getElementById('submit');
+submit.onclick = function(){
+    var request = new XMLHttpRequest();
+  
+    request.onreadystatechange = function(){
+    if(request.readyState == XMLHttpRequest.DONE){
+        //Take action
+        if(request.status == 200){
+            var counter = request.responseText;
+            var span = document.getElementById('span');
+            span.innerHTML = counter.toString();
+        }
+    }
+    //Do nothing
+  };
+  
+  //Make request 
+  request.open('GET','http://milanchhatralia.imad.hasura-app.io/counter', true);
+  request.send(null);
+};
