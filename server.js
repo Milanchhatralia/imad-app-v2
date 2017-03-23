f8a103b0670081a1463f8dcf2a0bc521b6218537
Milanchhatralia <milan.chhatralia@gmail.com>
2017-03-23 21:51:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -123,10 +123,23 @@ app.get('/submit-name',function(req, res){
    res.send(JSON.stringify(names));
 });
 
-app.get('/:articleName',function(req, res){
+app.get('/article/:articleName',function(req, res){
     //articleName = article-one
     //articles[articleName] = content object for aticle one
     var articleName = req.params.articleName;
+    
+    pool.query("SELECT * FROM article WHERE title = "+ req.params.articleName, function (err, result){
+        if(err){
+            res.send(500).send(err.toString());
+        } else{
+            if (result.rows.length === 0){
+                res.status(404).send('Article not found');
+            }else {
+                var articleData = result.rows[0];
+                res.send(createTemplate(articleData));
+            }
+        }
+    });
     res.send(createTemplate(article[articleName]));
 });
 
